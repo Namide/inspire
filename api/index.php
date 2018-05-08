e8c9d6591fd6574b07763fb35b8c5064be5fe7a5
Damien Doussaud (namide.com) <damien.git@gmail.com>
2018-05-08 08:23:40 +0200

    API > Check if config is created

--- api/index.php
+++ api/index.php
@@ -1,5 +1,15 @@
 <?php
 
-require_once __DIR__ . '/config.php';
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/system/app.php';
+$configFile = __DIR__ . '/config.php';
+if (file_exists($configFile))
+{
+    require_once $configFile;
+    require_once __DIR__ . '/vendor/autoload.php';
+    require_once __DIR__ . '/system/app.php';
+}
+else
+{
+    header('Content-Type: application/json');
+    echo '{"success":false,"message":"You must create the file api/config.php from the file api/config.sample.php"}';
+    exit(0);
+}
