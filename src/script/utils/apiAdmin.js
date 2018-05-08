8cb8de16ef7b3270432cb0ed0a8e60d736690257
Damien Doussaud (namide.com) <damien.git@gmail.com>
2018-05-08 08:19:32 +0200

    Admin > API > Move file reader

--- src/script/utils/apiAdmin.js
+++ src/script/utils/apiAdmin.js
@@ -1,5 +1,6 @@
 import config from '../../config'
 import { Api } from './api'
+import * as Vibrant from 'node-vibrant'
 
 const dataToFormData = data =>
 {
@@ -18,11 +19,159 @@ const dataToFormData = data =>
     return form
 }
 
+class Post
+{
+    construct()
+    {
+        this.complete = false
+        this.data = { }
+
+        this._onError = []
+        this._onComplete = []
+    }
+
+    addOnError(callback)
+    {
+        this._onError.push(callback)
+    }
+
+    removeOnError(callback)
+    {
+        const i = this._onError.indexOf(callback)
+        if (i > -1)
+            this._onError.splice(i, 1)
+    }
+
+    addOnComplete(callback)
+    {
+        this._onComplete.push(callback)
+    }
+
+    removeOnComplete(callback)
+    {
+        const i = this._onComplete.indexOf(callback)
+        if (i > -1)
+            this._onComplete.splice(i, 1)
+    }
+
+    addType(type)
+    {
+        if (!this.data.types)
+            this.data.types = []
+        
+        if (!this.data.type.find(t => t.toLowerCase() === type.toLowerCase()))
+            this.data.types.push(type)
+    }
+
+    addFile(file)
+    {
+        this.complete = false
+        const types = file.type.split('/')
+        types.forEach(type => this.addType(type))
+
+        this.data.content_file = {
+            name: file.name,
+            type: file.type,
+            size: file.size
+        }
+
+        if (file.name && file.name !== '' &&
+            (!this.data.title || this.data.title === ''))
+        {
+            const arr = file.name.split('.')
+            arr.pop()
+
+            const title = arr.join(' ')
+                .split('_').join(' ')
+                .split('-').join(' ')
+
+            this.data.title = title.substr(0,1).toUpperCase()
+                + title.substr(1, title.length)
+        }
+
+        const reader = new FileReader()
+        reader.addEventListener('load', event =>
+        {
+            const result = event.target.result
+
+            // Add base64
+            {
+                const base64Arr = result.split(';base64,')
+                base64Arr.shift()
+                this.file = {
+                    base64: base64Arr.join(';base64,')
+                }
+            }
+
+            // Add image data
+            const isImage = file.type.split('/').shift().toLowerCase() === 'image'
+            if (isImage)
+            {
+                this.file.src = result
+
+                var img = new Image()
+                img.addEventListener('load', event =>
+                {
+                    this.data.content_file.width = img.width
+                    this.data.content_file.height = img.height
+
+                    // this.fileImgClampW = img.width > img.height
+
+                    extractColors(img, colors =>
+                    {
+                        this.data.content_file.colors = colors
+                        this.complete = true
+                        this._onComplete.forEach(callback => callback(this))
+                    }, error =>
+                    {
+                        this.complete = true
+                        this._onError.forEach(callback => callback(this, error))
+                    })
+                })
+
+                // img.src = result
+            }
+            else
+            {
+                this.complete = true
+                this._onComplete.forEach(callback => callback(this))
+            }
+        })
+
+        reader.readAsDataURL(file)
+    }
+
+    updateData()
+    {
+        if (this.complete)
+        {
+            
+        }
+        else
+        {
+            this._onComplete.push(() =>
+            {
+                this.updateData()
+            })
+        }
+    }
+}
+
 class ApiAdmin extends Api
 {
     constructor()
     {
         super()
+    }
+
+    createPost()
+    {
+        return new Post()
+    }
+
+    sendPost(post)
+    {
+
     }
 
     addPost(onload, data)
@@ -69,6 +218,40 @@ class ApiAdmin extends Api
     }
 }
 
+const extractColors = (img, onColors, onError) =>
+{
+    const options = {
+        // colorCount: number
+        // quality: 2
+        maxDimension: 1024
+        // filters: Array<Filter>
+        // ImageClass: ImageClass
+        // quantizer: Quantizer
+        // generator?: Generator
+    }
+    const vibrant = new Vibrant(img, options)
+    vibrant.getPalette((err, palette) =>
+    {
+        const colors = []
+        if (palette.LightVibrant)
+            colors.push(palette.LightVibrant.getHex())
+        if (palette.Vibrant)
+            colors.push(palette.Vibrant.getHex())
+        if (palette.DarkVibrant)
+            colors.push(palette.DarkVibrant.getHex())
+        if (palette.LightMuted)
+            colors.push(palette.LightMuted.getHex())
+        if (palette.Muted)
+            colors.push(palette.Muted.getHex())
+        if (palette.DarkMuted)
+            colors.push(palette.DarkMuted.getHex())
+        
+        onColors(colors)
+
+        if (err)
+            onError(err)
+    })
+}
 
 const apiAdmin = new ApiAdmin()
 
