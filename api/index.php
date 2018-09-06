b57c7f95a383fdba70e7bf54c79d833cef06d197
Namide <damien.git@gmail.com>
2018-09-06 13:55:10 +0200

    API > Install > Detect bad install

--- api/index.php
+++ api/index.php
@@ -1,12 +1,37 @@
 <?php
+
 define('START_TIME', microtime(true));
 $configFile = __DIR__.'/config.php';
+
+// Config is created
 if (file_exists($configFile)) {
     require_once $configFile;
-    require_once __DIR__.'/vendor/autoload.php';
-    require_once __DIR__.'/system/app.php';
+
+    // Composer is initialized
+    if (file_exists(__DIR__.'/vendor/autoload.php')) {
+        require_once __DIR__.'/vendor/autoload.php';
+        require_once __DIR__.'/system/app.php';
+
+    // Composer not initialized
+    } else {
+        if (strstr($_SERVER['REQUEST_URI'], 'api/config.js') !== false) {
+            header('Content-Type: application/javascript');
+            echo 'alert("You must install composer.phar in the /api directory and run the command \"php composer.phar install\" or \"composer install\");';
+        } else {
+            header('Content-Type: application/json');
+            echo '{"success":false,"message":"You must install composer.phar in the /api directory and run the command \"php composer.phar install\""}';
+        }
+        exit(0);
+    }
+
+// Config not found
 } else {
-    header('Content-Type: application/json');
-    echo '{"success":false,"message":"You must create the file api/config.php from the file api/config.sample.php"}';
+    if (strstr($_SERVER['REQUEST_URI'], 'api/config.js') !== false) {
+        header('Content-Type: application/javascript');
+        echo 'alert("You must create the file api/config.php from the file api/config.sample.php");';
+    } else {
+        header('Content-Type: application/json');
+        echo '{"success":false,"message":"You must create the file api/config.php from the file api/config.sample.php"}';
+    }
     exit(0);
 }
