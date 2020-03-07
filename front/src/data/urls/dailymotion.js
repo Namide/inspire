b0bcba36dcc4ec747c40ed6408ef6f41b1b09977
Namide <damien.git@gmail.com>
2020-03-07 11:51:18 +0100

    refactor: split distant url analyser

---
+++ front/src/data/urls/dailymotion.js
@@ -0,0 +1,51 @@
+export default [{
+  regexList: [/dailymotion\.com\/video\/[a-z0-1]+/i],
+  process: (url, { apiSave }) => {
+    return new Promise(resolve => {
+      const video = url.pathname.split('/')[2]
+      const dataURL = new URL('https://www.dailymotion.com/services/oembed?url=' + encodeURIComponent(url.href))
+      apiSave.getDistantLink(dataURL.href)
+        .then(response => {
+          if (response.ok) {
+            // console.log(dataURL.href)
+            // console.log(response)
+            return response.json()
+          } else {
+            throw new Error('Link not found')
+          }
+        })
+        .then(json => {
+          resolve({
+            title: json.title,
+            description: json.description,
+            types: ['embed', 'video'],
+            tags: ['video', json.author_name],
+            contentObject: {
+              type: 'embed',
+              url: url.url,
+              width: json.width,
+              height: json.height,
+              image: json.thumbnail_url,
+              author: json.author_name,
+              raw: `<iframe frameborder="0" width="${json.width}" height="${json.height}" src="https://www.dailymotion.com/embed/video/${video}" allowfullscreen allow="autoplay"></iframe>`
+            }
+          })
+        })
+        .catch(error => {
+          console.error('dailymotion error: ' + error.message)
+          resolve({
+            types: ['embed', 'video'],
+            tags: ['video'],
+            contentObject: {
+              type: 'embed',
+              url: url.href,
+              width: 640,
+              height: 360,
+              raw: `<iframe frameborder="0" width="640" height="360" src="https://www.dailymotion.com/embed/video/${video}" allowfullscreen allow="autoplay"></iframe>
+              `
+            }
+          })
+        })
+    })
+  }
+}]
---
+++ front/src/data/urls/vimeo.js
@@ -0,0 +1,48 @@
+export default [{
+  regexList: [/vimeo\.com\/(\d+)/ig],
+  process: (url, { apiSave }) => {
+    return new Promise(resolve => {
+      const video = url.pathname.split('/')[1]
+      const dataURL = new URL('https://vimeo.com/api/v2/video/' + video + '.json')
+      apiSave.getDistantLink(dataURL.href)
+        .then(response => {
+          if (response.ok) {
+            return response.json()
+          } else {
+            throw new Error('Link not found')
+          }
+        })
+        .then(([json]) => {
+          resolve({
+            title: json.title,
+            description: json.description.split('<br />').join(' '),
+            tags: ['video', json.user_name],
+            types: ['embed', 'video'],
+            contentObject: {
+              type: 'embed',
+              url: url.url,
+              width: json.width,
+              height: json.height,
+              image: json.thumbnail_large,
+              author: json.user_name,
+              raw: `<iframe src="https://player.vimeo.com/video/${video}" width="${json.width}" height="${json.height}" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`
+            }
+          })
+        })
+        .catch(error => {
+          console.error('vimeo error: ' + error.message)
+          resolve({
+            types: ['embed', 'video'],
+            tags: ['video'],
+            contentObject: {
+              type: 'embed',
+              url: url.href,
+              width: 640,
+              height: 360,
+              raw: `<iframe src="https://player.vimeo.com/video/${video}" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>`
+            }
+          })
+        })
+    })
+  }
+}]
