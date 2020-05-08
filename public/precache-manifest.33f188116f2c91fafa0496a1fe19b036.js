513ad999f430e6b1738c469d23d0fe776a1fc22d
Namide <damien.git@gmail.com>
2020-05-08 16:36:17 +0200

    Build content

---
+++ public/precache-manifest.33f188116f2c91fafa0496a1fe19b036.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0037624e2681a47af759",
+    "url": "/css/admin.da5eb05d.css"
+  },
+  {
+    "revision": "21fac351db30e8de984a",
+    "url": "/css/app.560cabd0.css"
+  },
+  {
+    "revision": "796be296af83765908fe",
+    "url": "/css/import.d2a814b1.css"
+  },
+  {
+    "revision": "7e1eda3a103b1d4d14c9dea4ced21bfb",
+    "url": "/fonts/inter.var.7e1eda3a.woff2"
+  },
+  {
+    "revision": "24a0bbf50846ed06eb6c6a90df769bfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0037624e2681a47af759",
+    "url": "/js/admin.8e24e9c9.js"
+  },
+  {
+    "revision": "5c1c50a6355537220be8",
+    "url": "/js/admin~import.30810cae.js"
+  },
+  {
+    "revision": "21fac351db30e8de984a",
+    "url": "/js/app.d9ee41f6.js"
+  },
+  {
+    "revision": "23f6b9b329a0209fe532",
+    "url": "/js/chunk-vendors.38525e80.js"
+  },
+  {
+    "revision": "796be296af83765908fe",
+    "url": "/js/import.bee7a2ca.js"
+  },
+  {
+    "revision": "a2a67c1c032709519dfe21ddbdf28b8f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
